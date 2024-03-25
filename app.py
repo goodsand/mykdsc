056de0f3cc6b611a5d0e11368fcc07f9d47d8d2b
goodsand <goodsand@163.com>
2024-03-25 10:18:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
-import os
+from flask import Flask,request
 
-app = Flask(__name__)
+import urllib
+import json
+app=Flask(__name__)
 
-@app.route('/')
-def index():
-    print("GET /")
-    return "Welcome, this is a Flask app deployed on Zeabur"
+@app.route('/xf299',methods=['POST'])
+def handle_post():
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
+    out="续费加299："
+    out+="\n号码："+request.form.get('num')
+    out+="\n机主："+request.form.get('name')
+    out+="\n联电："+request.form.get('phone')
+    out+="\n证号："+request.form.get('pid')
+    out+="\n============\n"
+
+    url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=758f2d15-0de0-4b4f-b41f-16a70a9b4fea"  # 这里就是群机器人的Webhook地址
+    headers={'Content-Type':'application/json'}
+    data={"msgtype": "text","text": { "content": out} }
+    data=bytes(json.dumps(data),'utf8')
+
+    requesta=urllib.request.Request(url=url,data=data,headers=headers)
+    response=urllib.request.urlopen(requesta)
+    content=response.read().decode('utf-8')
+    obj=json.loads(content)
+    obj=json.dumps(obj).encode('utf-8')
+    return obj
+if __name__=='__main__':
+    app.run()
